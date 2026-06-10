ad8af8e09b5d8bc9d8638d179ddb3408c60b2d61
جواد <89310596+jjrhrh@users.noreply.github.com>
2026-06-10 08:19:16 +0300

    Update isSiteReachable.ts

--- src/lib/isSiteReachable.ts
+++ src/lib/isSiteReachable.ts
@@ -1,8 +1,3 @@
 export const isSiteReachable = async (url: string): Promise<boolean> => {
-  try {
-    const response = await fetch(url, { method: 'HEAD' });
-    return response.ok;
-  } catch (error) {
-    return false;
-  }
-}
+  return true;
+};
